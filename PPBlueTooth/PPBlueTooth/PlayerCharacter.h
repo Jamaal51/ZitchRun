efac8744f11318248c62195a8ab2e4225ba228d2
Jamaal Sedayao <you@example.com>
2015-11-14 15:07:45 -0500

    first commit

---
+++ PPBlueTooth/PPBlueTooth/PlayerCharacter.h
@@ -0,0 +1,26 @@
+//
+//  PlayerCharacter.h
+//  PPBlueTooth
+//
+//  Created by Fatima Zenine Villanueva on 11/12/15.
+//  Copyright © 2015 apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerCharacter : NSObject
+
+@property (nonatomic) BOOL virus;
+@property (nonatomic) BOOL infected;
+@property (nonatomic) BOOL healthy;
+@property (nonatomic, strong) NSString *userName;
+@property (nonatomic, strong) NSString *zitronUserName;
+
+@end
+
+
+
+
+
+
+
